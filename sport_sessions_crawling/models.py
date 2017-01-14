6bdd03d2d86c2afd7f742fe2f0672bc946b786ab
Korrigan <rainrider37@gmail.com>
2017-01-15 00:36:13 +0100

    Update model

--- sport_sessions_crawling/models.py
+++ sport_sessions_crawling/models.py
@@ -1,24 +1,51 @@
+from contextlib import contextmanager
 from sqlalchemy import *
 from sqlalchemy.engine.url import URL
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 import sport_sessions_crawling.settings as settings
 
 DeclarativeBase = declarative_base()
 
-def connect():
+class db(object):
     """
     Performs database connection using database settings from settings.py.
     Returns sqlalchemy engine instance
     """
-    return create_engine(URL(**settings.DATABASE))
+    __shared_state = {}
+
+    def __init__ (self):
+        self.__dict__ = self.__shared_state
+        if not hasattr(self, 'engine'):
+            self.engine = create_engine(URL(**settings.DATABASE))
+        if not hasattr(self, 'Session'):
+            self.session = sessionmaker(bind=self.engine)()
+
 
 def create_tables(engine):
     """
-    Creating our tables
+    Creating our tables if they're not included in database
     """
-    DeclarativeBase.metadata.create_all(engine, checkfirst=True)
+    DeclarativeBase.metadata.create_all(engine)
+
+class Session(DeclarativeBase):
+    __tablename__ = 'Session'
+
+    id = Column(Integer, primary_key=True)
+    energy = Column('energy', Integer)
+    average_speed = Column('average_speed', Float)
+    duration = Column('duration', Time)
+    date = Column('date', DateTime)
+    comment = Column('comment', String)
+    average_heatrate = Column('average_heartrate', Integer)
+    max_heartrate = Column('max_heartrate', Integer)
+    id_track = Column(Integer, ForeignKey('track.id'))
+    id_source = Column(Integer, ForeignKey('source.id'))
+    id_sport = Column(Integer, ForeignKey('sport.id'))
+    id_event = Column(Integer, ForeignKey('event.id'))
+    id_user = Column(Integer, ForeignKey('user.id'))
+
 
 class Event(DeclarativeBase):
     __tablename__ = "event"
@@ -29,14 +56,14 @@ class Event(DeclarativeBase):
     location = Column('location', String, nullable=True)
     date = Column('date', DateTime, nullable=True)
     length = Column('length', Integer)
-    session = relationship('Session')
+    session = relationship('Session', backref="event")
 
 class Sport(DeclarativeBase):
     __tablename__ = "sport"
 
     id = Column(Integer, primary_key=True)
     name = Column('name', String)
-    session = relationship('Session')
+    session = relationship('Session', backref="sport")
 
 class Source(DeclarativeBase):
     __tablename__ = "source"
@@ -44,27 +71,23 @@ class Source(DeclarativeBase):
     id = Column(Integer, primary_key=True)
     name = Column('name', String)
     url = Column('url', String)
-    session = relationship('Session')
+    session = relationship('Session', backref="source")
 
 class Track(DeclarativeBase):
     __tablename__ = "track"
 
     id = Column(Integer, primary_key=True)
     filetype = Column('filetype', String)
-    session = relationship('Session')
     elevation = Column('elevation', Integer)
     distance = Column('distance', Float)
-    content = Column('content', BLOB)
+    content = Column('content', Binary)
+    session = relationship('Session', backref="track")
 
-class Session(DeclarativeBase):
-    __tablename__ = "session"
+class User(DeclarativeBase):
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
-    energy = Column('energy', Integer)
-    average_speed = Column('average_speed', Float)
-    duration = Column('duration', Time)
-    date = Column('date', DateTime)
-    id_track = Column(Integer, ForeignKey('track.id'))
-    id_source = Column(Integer, ForeignKey('source.id'))
-    id_sport = Column(Integer, ForeignKey('sport.id'))
-    id_event = Column(Integer, ForeignKey('event.id'))
+    name = Column('name', String)
+    age = Column('age', Integer)
+
+    session = relationship('Session', backref='user')
